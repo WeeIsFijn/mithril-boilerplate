7cfa736810b04a49bf5a04219db2eb752888bd41
Wouter Deferme <wouterd@intouch.be>
2018-02-21 14:55:14 +0100

    example ES6 component

--- src/index.js
+++ src/index.js
@@ -2,4 +2,21 @@ import  m  from 'mithril';
 
 let loaded = 'loaded';
 
-m.render(document.body, <h1>Mithril {loaded}</h1> );
+export class Hello {
+    constructor() {
+        this.count = 0;
+    }
+
+    increment = () => { console.log('incrementing'); this.count = this.count + 1;}
+    
+    view() {
+        return (
+            <span>
+                <h1>Component {this.count}</h1>
+                <button onclick={ this.increment }>count up</button>
+            </span>
+        );
+    }
+}
+
+m.mount(document.body, Hello );
